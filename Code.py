82e42bcb05673a9790dabe6efb72bc95145ea99b
MayuriChourasia <102643897+MayuriChourasia@users.noreply.github.com>
2022-04-01 15:46:27 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -15,19 +15,12 @@ def get_slope(arr2,arr3):
 
 def perpendicular_slope(arr2,arr3):
     s_perp=get_slope(arr2,arr3)[::-1]
-    s_perp[0]=-(s_perp[0])
+    s_perp[1]=-(s_perp[1])
     return s_perp
     #slope of perpendicular= s_per[0]/s_perp[1]
     '''we have got the slope of the perpencicular, now we will buit a function to find the constant 
     in the general equation of line i.e. y=mx+c,using the co-ordinates of point through which 
     the line passes and the slope of the perpencicular'''
-
-def finding_constant(arr1,m):
-    #manipulating the general equation to find constant; c=y-mx
-    constant=[]
-    constant.append(m[1]*arr1[1]-m[0]*(arr1[0]))
-    constant.append(m[1])
-    return constant
 
 A=np.array([3,8])
 B=np.array([-1,2])
@@ -40,8 +33,9 @@ print('i) The slope of line BC is:',slope[0],'/',slope[1],'\n')
 
 #for the second subquestion
 s=perpendicular_slope(B,C)
-c=finding_constant(A,s)
 print('ii) The equation of line perpendicular to BC and passing')
-print('through the point A is given by:',c[1]*s[1],'y =',c[1]*s[0],'x +',c[0]*s[1])
+'''For a line passing through a point (𝑥1,𝑦1) and having a slope m can be given 
+   by the equation : ( y - 𝑦1) = m ( x - 𝑥1)'''
+print('through the point A is given by:',s[1],'y -',s[0],'x  =',s[1]*A[1]- s[0]*A[0])
 
 #end
