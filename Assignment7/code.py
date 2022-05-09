699f042c36b1f837d46d7f36cf16bc64f7bb8d87
MayuriChourasia <102643897+MayuriChourasia@users.noreply.github.com>
2022-05-09 21:35:00 +0530

    Create code.py

---
+++ Assignment7/code.py
@@ -0,0 +1,11 @@
+import random
+p_a = random.random() #obtains random float between 0 and 1
+p_b = random.random()
+p1 = p_a*p_b
+p2 = p_a+p_b-p1
+p3 = p1/p_b
+p4 = p1/p_a
+print(p1)
+print(p2)
+print(p3)
+print(p4)
